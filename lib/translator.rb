5aa731bfee735ef09844dc4f6016b58cff55788f
Jason Melton <jason.melton2@gmail.com>
2020-01-29 19:44:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -34,7 +34,11 @@ end
 def get_english_meaning(path, emoticon)
   library = load_library(path)
   library[:get_meaning][emoticon]
-
+  if library[:get_emoticon][emoticon]
+     library[:get_emoticon][emoticon]
+  else 
+     "Sorry, that emoticon was not found"
+end 
   
 
 end
