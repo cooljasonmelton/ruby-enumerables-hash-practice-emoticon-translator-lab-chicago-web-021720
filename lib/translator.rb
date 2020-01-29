d59dd1fb90bf08b19415c10a827e88b9aaf2ab7a
Jason Melton <jason.melton2@gmail.com>
2020-01-29 19:26:17 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,20 @@
-# require modules here
+require "yaml"
+require "pp"
 
-def load_library
-  # code goes here
+def load_library(path)
+  library = YAML.load_file(path)
+
+  
+  
+  
+  #  sort_library = library.reduce({}) do |memo, (keys, values)|
+  #  memo[:get_meaning] = k
+  #  memo[:get_emoticon] = v
+  #  memo
+  #  end 
+  #  sort_library
+
+
 end
 
 def get_japanese_emoticon
